10593c984b1e09e3ceec56bf6bef84146b203c45
Morgan Harvey <108724899+Alumicr@users.noreply.github.com>
2023-03-01 05:20:49 +0000

    Finshed player movement

--- script.js
+++ script.js
@@ -6,14 +6,10 @@ function setup() {
   cnv = new Canvas(windowWidth, windowHeight);
   player = new Sprite(300, 200, 50, "d");
   player.shapeColor = color("white");
-
-
-
   walls();
 
-  // player movement
+  // player movement when key is pressed
   document.addEventListener("keydown", function(event) {
-
     if (event.code == 'ArrowUp') {
       player.vel.y = -5;
     }
@@ -27,15 +23,21 @@ function setup() {
       player.vel.x = 5;
     }
   })
+  
+  // player movement reset when key is relased 
+  document.addEventListener("keyup", function(event) {
 
-// FINSH MOVEMENT
-
+    if (event.code == "ArrowUp" || event.code == "ArrowDown") {
+      player.vel.y = 0;
+    }
+    else if (event.code == "ArrowLeft" || event.code == "ArrowRight") {
+      player.vel.x = 0;
+    }
+  })
 }
 
-
-
 function walls() {
-  // function makes walls and colours
+  // function makes walls and sets the colours
   wallRH = new Sprite(width, height / 2, 8, height, 'k');
   wallRH.shapeColor = color('white');
   wallLH = new Sprite(0, height / 2, 8, height, 'k');
@@ -44,11 +46,13 @@ function walls() {
   wallTop.shapeColor = color('white');
   wallBot = new Sprite(width, height + 8, width * 2, 8, 'k');
   wallBot.shapeColor = color('white');
-
 }
 
 function draw() {
+  //background for canvas
   background("black");
 
 
 }
+
+//end of code 
